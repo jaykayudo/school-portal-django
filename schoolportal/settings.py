d2ef6b558ebf71caed545214e156cb4e5994d1ba
joshua <jaykayudo@gmail.com>
2022-05-18 02:06:17 +0100

    made changes

--- schoolportal/settings.py
+++ schoolportal/settings.py
@@ -105,6 +105,6 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'main/static'),os.path.join(BASE_DIR,'staff/static'),os.path.join(BASE_DIR,'admin/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'main/static'),os.path.join(BASE_DIR,'staff/static'),os.path.join(BASE_DIR,'admins/static')]
 LOGIN_URL = '/login'
 LOGIN_REDIRECT_URL = '/login'
